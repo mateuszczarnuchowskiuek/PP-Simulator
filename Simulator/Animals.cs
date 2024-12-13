5fa7d5add6de8c12fa5047ae6250ad1faa12a553
234189@student.uek.krakow.pl <234189@student.uek.krakow.pl>
2024-12-13 02:14:42 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,8 +1,42 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string description = "Unknown";
+    public required string Description
+    {
+        get { return description; }
+        init
+        {
+            if (value == null)
+                description = "Unknown";
+            else
+            {
+                description = value.Trim();
+                if (description.Length == 0)
+                    description = "Unknown";
+                if (description.Length == 1)
+                    description += "##";
+                else if (description.Length == 2)
+                    description += "#";
+                else if (description.Length > 15)
+                    description = description[..15];
+                description = description.Trim();
+
+                if (description.Length == 0)
+                    description = "Unknown";
+                if (description.Length == 1)
+                    description += "##";
+                else if (description.Length == 2)
+                    description += "#";
+
+                description = description[0].ToString().ToUpper() + description[1..];
+            }
+        }
+    }
     public uint Size { get; set; } = 3;
     public string Info
     {
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,13 +2,57 @@ namespace Simulator;
 
 public class Creature
 {
-    public string? Name { get; set; }
-    public int Level { get; set; }
-
-    public Creature(string name, int level = 1)
+    private string name = "Unknown";
+    public string Name
     {
-        this.Name = name;
-        this.Level = level;
+        get { return name; }
+        init
+        {
+            if (value == null)
+                name = "Unknown";
+            else
+            {
+                name = value.Trim();
+                if (name.Length == 0)
+                    name = "Unknown";
+                if (name.Length == 1)
+                    name += "##";
+                else if (name.Length == 2)
+                    name += "#";
+                else if (name.Length > 25)
+                    name = name[..25];
+                name = name.Trim();
+
+                if (name.Length == 0)
+                    name = "Unknown";
+                if (name.Length == 1)
+                    name += "##";
+                else if (name.Length == 2)
+                    name += "#";
+
+                name = name[0].ToString().ToUpper() + name[1..];
+            }
+        }
+    }
+    private int level = 1;
+    public int Level
+    {
+        get { return level; }
+        init
+        {
+            if (value < 1)
+                level = 1;
+            else if (value > 10)
+                level = 10;
+            else
+                level = value;
+        }
+    }
+
+    public Creature(string Name, int Level = 1)
+    {
+        this.Name = Name;
+        this.Level = Level;
     }
     public Creature()
     {
@@ -22,6 +66,13 @@ public class Creature
     public string Info
     {
         get { return $"{this.Name} [{this.Level}]"; }
+    }
+
+    public void Upgrade()
+    {
+        level += 1;
+        if (level > 10)
+            level = 10;
     }
 
 }
